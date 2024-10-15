b76f535dc75c21a1acc1550aa0703fd7a8134728
Hanna Makhnovich <anya.makhnovich@gmail.com>
2024-10-15 15:05:18 +0300

    Lesson 31 (add onClick functions for buttons)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,17 @@
 import style from "./Dialogs.module.css"
 import Dialog from "./Dialog/Dialog";
 import Message from "./Message/Message";
+import {createRef} from "react";
+
 function Dialogs({state}) {
     let dialogsElem = state.dialogs.map(dialog => <Dialog name={dialog.name} id={dialog.id}/>);
     let messagesElem = state.messages.map(message => <Message messageText={message.text}/>);
+
+    let component = createRef();
+
+    let sendMessage = () => {
+        alert(component.current.value);
+    }
 
     return (
         <div className={style.dialogs + " block"}>
@@ -12,7 +20,13 @@ function Dialogs({state}) {
             </div>
             <div className={style.messages}>
                 {messagesElem}
+
+                <div className={style.newMessage}>
+                    <textarea ref={component}/>
+                    <button onClick={sendMessage}>Send</button>
+                </div>
             </div>
+
         </div>
     );
 }
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,20 @@
 import style from "./MyPosts.module.css"
 import Post from "./Post/Post";
+import {createRef} from "react";
+
 function MyPosts({state}) {
     let postsElem = state.posts.map(post => <Post postText={post.text} likesCount={post.likes}/>)
+    let component = createRef();
+
+    let addPost = () => {
+        alert(component.current.value);
+    }
 
     return (
         <div className={style.content}>
             <div className={style.newPost}>
-                <textarea />
-                <button>Add post</button>
+                <textarea ref={component}/>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className={style.posts}>
                 {postsElem}
